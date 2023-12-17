5204903e4504212754e4c65122f580a36fb7b0bf
afanasenko_sergey <afanasenko.se@gmail.com>
2023-12-17 21:00:28 +0300

    Add

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255 )
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.question.question_text} - {self.choice_text}'
